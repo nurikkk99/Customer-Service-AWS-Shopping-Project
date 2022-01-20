364b2786100796fae96740b78b41ebe84893b809
Nurmyrat Tagandurdyyev <Nurmyrat_Tagandurdyyev@epam.com>
2022-01-20 12:27:51 +0300

    Add remote aws services support

--- src/main/java/com/epam/customerservice/helper/Indices.java
+++ src/main/java/com/epam/customerservice/helper/Indices.java
@@ -1,5 +1,5 @@
 package com.epam.customerservice.helper;
 
 public class Indices {
-    public static final String PRODUCT_INDEX = "product";
+    public static final String PRODUCT_INDEX = "products";
 }
